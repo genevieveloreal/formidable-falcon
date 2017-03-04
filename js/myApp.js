b29fc892acd65becafa33543783271b419f923cb
genevieveloreal <genevieveloreal@gmail.com>
2017-03-04 11:19:39 +1000

    Fixed issues on index.html page (picture thumbnails)

--- js/myApp.js
+++ js/myApp.js
@@ -14,7 +14,7 @@ let agents = [
     }, {
         name: "Matthew",
         codeName: "Courageous Condor",
-        img: "https://placehold.it/200x200",
+        img: "images/genevieve.jpg",
         html: "profiles/matthew-burfield.html"
     }, {
         name: "Nimai",
@@ -38,7 +38,7 @@ function loadAgents() {
     let agentsList =  document.getElementById('agentsList');
     agents.forEach(function(agent){
         let agentHtml = document.createElement('div');
-        agentHtml.classList.add("col-md-3", "col-sm-6");
+        agentHtml.classList.add("col-md-3", "col-sm-6", "col-xs-6");
         agentHtml.innerHTML = `<a href=" ${ agent.html } ">
         <img src=" ${ agent.img } " class="fade" alt=" ${ agent.codeName } "></a>`;
         agentsList.appendChild(agentHtml);
