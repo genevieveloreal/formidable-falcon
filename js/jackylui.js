5a10f55e49ef7b2e06766418fd59533dce7f11d4
JackyRecelis <jackymlui@live.com>
2017-03-04 11:38:13 +1000

    jackylui.js

--- js/jackylui.js
+++ js/jackylui.js
@@ -2,3 +2,6 @@ for (var ii =0; ii < agents.length; ii++){
     var id = agents[ii].name;
     document.getElementById(id).innerText = agents[ii].codeName;
 }
+
+document.getElementById("imageBackSource").src = agents[1].imgBackground;
+document.getElementById("JackyPic").src = agents[1].img;
