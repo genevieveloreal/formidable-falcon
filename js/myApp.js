30b3e117b2fe37263334f11d9a93c23bcb4575be
Greg Duncan <gregdd@outlook.com>
2017-03-06 14:21:37 +1000

    instructions added my button

--- js/myApp.js
+++ js/myApp.js
@@ -54,7 +54,6 @@ let view = {
             agentsImageDiv.appendChild(agentHtml);
         });
         agentsImageDiv.classList.add('pictures', 'row', 'clearfix');
-        debugger;
         agentsImageDiv.insertAdjacentHTML( 'beforeend', '<br><p class="text-center instructions-link"><button onclick="handlers.loadInstructions()"> >>> View the instructions <<< </button></p>');
         agentsList.innerHTML = '';
         agentsList.appendChild(agentsImageDiv); 
@@ -67,6 +66,33 @@ let view = {
             myLi.innerHTML = `<a href="${ agent.html }"> ${ agent.codeName } </a> `;
             myUl.appendChild(myLi);
         });
+    },
+    loadInstructions: function() {
+        document.querySelector('title').innerText = 'Instructions';
+        let instructionsEle = document.getElementById('pageInfo');
+        instructionsEle.innerHTML = ` 
+                    <h2><b>Instructions</b></h2>
+                    <p>You will need a github account and git installed on your computer. To contribute to the project <a href="profiles/genevievecarter.html">scaryCanary</a> will need to add you as a contributor.</p>
+                    <ol class="instructions-list">
+                        <li>Ensure you have an up to date version of the Formidable Falcon project on your computer</li>
+                        <li>Create a new .html file in the profiles folder for your profile page</li>
+                        <li>Add a link to your profile page and an image of yourself below</li>
+                        <li>You will need to create your own css file in the 'css' folder titled with your name</li>
+                        <li>Add information and style your profile page</li>
+                        <li>Information you will need to include:
+                            <ul>
+                                <li>Your code name</li>
+                                <li>A link to your github account</li>
+                                <li>A photo of yourself</li>
+                                <li>A description</li>
+                                <li>Hobbies, or favourite sayings</li>
+                            </ul>
+                        </li>
+                        <li>Commit your changes to the Formidable Falcon project</li>
+                        <li>Ask for feedback on your profile page!</li>
+                    </ol>
+                    <p><b>Bonus Challenge!</b></p>
+                    <p>For an added usability challenge, provide a way for the user to navigate back to the home page for Formidable Falcon, as well as a way to view the other member's pages.</p>`;
     }
 }
 
